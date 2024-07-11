5a9fa0d85b2be5e38b663b967f55670713fd4251
erikgomessantos <97444977+erikgomessantos@users.noreply.github.com>
2024-07-11 19:42:56 -0300

    Criando e testando requisição do tipo POST

--- routes/services.js
+++ routes/services.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const serviceController = require("../controllers/serviceController");
 
-
+// Endpoints
+router
+ .route("/services")
+ .post((req, res) => serviceController.create(req, res));
 
 module.exports = router;
