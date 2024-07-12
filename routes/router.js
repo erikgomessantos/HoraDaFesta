3cbcbb65eaea3ce0e51befe5d387b4e735fbb36a
erikgomessantos <97444977+erikgomessantos@users.noreply.github.com>
2024-07-12 05:41:31 -0300

    Configuração do arquivo de rotas das festas

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
 const servicesRouter = require("./services");
 
+// Services router
 router.use("/", servicesRouter);
+
+// Parties router
+const partyRouter = require("./parties");
+
+router.use("/", partyRouter);
 
 module.exports = router;
