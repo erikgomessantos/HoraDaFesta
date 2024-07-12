f82be22f235dea3266835d655e979063b0ffc5c4
erikgomessantos <97444977+erikgomessantos@users.noreply.github.com>
2024-07-12 05:04:04 -0300

    Criando e Testando Requisição do tipo Get

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -17,11 +17,19 @@ const serviceController = {
             res.status(201).json({response, msg: "Serviço criado com Sucesso!"});
 
         } catch (error) {
-
             console.log(error)
-
         }
-    } 
+    },
+    getAll: async(req, res) => {
+        try {
+            const services = await ServiceModel.find();
+
+            res.json(services);
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
 };
 
 module.exports = serviceController;
