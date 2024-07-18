e26904bb1cb9fb55f044a61d39c6f9ef82b404c6
erikgomessantos <97444977+erikgomessantos@users.noreply.github.com>
2024-07-17 21:00:08 -0300

    Inclusão dos serviços e ajuste da página de criação de festas

--- vite-project/src/routes/CreateParty.jsx
+++ vite-project/src/routes/CreateParty.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
 const CreateParty = () => {
-    return <div>CreateParty</div>;
+    return <div className="form-page">
+        <h2>Crie sua próxima Festa</h2>
+        <p>Defina o seu orçamento e escolha os serviços</p>
+        <form>
+            <label>
+                <span>Nome da Festa:</span>
+                <input type="text" placeholder="Seja Criativo..." required/>
+            </label>
+            <label>
+                <span>Anfitrião:</span>
+                <input type="text" placeholder="Quem está dando a Festa?" required/>
+            </label>
+            <label>
+                <span>Descrição:</span>
+                <textarea placeholder="Conte mais sobre a Festa..." required></textarea>
+            </label>
+            <label>
+                <span>Orçamento:</span>
+                <input type="number" placeholder="Quanto você pretende Investir?" required/>
+            </label>
+            <label>
+                <span>Imagem:</span>
+                <input type="text" placeholder="Insira a URL de uma Imagem" required/>
+            </label>
+            <div>
+                <h2>Escolha os serviços</h2>
+                <div className="services-container">
+                    <p>Serviços...</p>
+                </div>
+            </div>
+            <input type="submit" value="Criar Festa" className="btn" />
+        </form>
+    </div>;
 };
 
 export default CreateParty;
