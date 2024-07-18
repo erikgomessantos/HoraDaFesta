dc677ad67a05ea00f6cbb433c9ec3105f10caa56
erikgomessantos <97444977+erikgomessantos@users.noreply.github.com>
2024-07-18 19:57:12 -0300

    CSS da página de criação de festas

--- vite-project/src/routes/CreateParty.jsx
+++ vite-project/src/routes/CreateParty.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import partyFetch from "../axios/config";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import "./Form.css";
 
 const CreateParty = () => {
 
