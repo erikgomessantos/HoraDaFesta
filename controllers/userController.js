637dbe8fe792971bf104574d058c61cc351d5405
erikgomessantos <97444977+erikgomessantos@users.noreply.github.com>
2024-07-29 21:56:31 -0300

    Incluindo Funcionalidades de Atualizar, Excluir e Consultar Usuários

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,7 +24,64 @@ const userController = {
 
         res.json(users);
       } catch (error) {
+        console.log(error);
+      }
+    },
+    get: async(req, res) => {
+      try {
         
+        const id = req.params.id;
+        const user = await UserModel.findById(id);
+
+        if(!user) {
+          res.status(404).json({msg: "Usuário não Encontrado"});
+          return;
+        }
+
+        res.json(user);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    delete: async(req, res) => {
+      try {
+
+        const id = req.params.id
+        const user = await UserModel.findById(id);
+
+        if(!user) {
+          res.status(404).json({msg: "Usuário não Encontrado"});
+          return;
+        }
+
+        const deletedUser = await UserModel.findByIdAndDelete(id);
+
+        res.status(200).json({deletedUser, msg: "Usuário Excluído com Sucesso"});
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    update: async(req, res) => {
+      try {
+        
+        const id = req.params.id
+        
+        const user = {
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+        };
+
+        const updatedUser = await UserModel.findByIdAndUpdate(id, user);
+
+        if(!user) {
+          res.status(404).json({msg: "Usuário não Encontrado"});
+          return;
+        }
+
+        res.status(200).json({updatedUser, msg: "Usuário Atualizado com Sucesso"});
+      } catch (error) {
+        console.log(error);
       }
     }
 };
