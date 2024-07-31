e02652197a133c95039bae1173c52e0cdaecbe53
erikgomessantos <97444977+erikgomessantos@users.noreply.github.com>
2024-07-31 20:44:26 -0300

    Ajuste de Funcionalidade para carregamentos de Usuários

--- HoraDaFestaFront/src/routes/Users.jsx
+++ HoraDaFestaFront/src/routes/Users.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import partyFetch from "../axios/config"; // Comunicação com API
+import { useState, useEffect } from "react"; // Salvar os dados e Requisição na API
+import { Link } from "react-router-dom"; // Adicionar o Link para a página de Uusários Individuais
 
 const Users = () => {
-    return <div>Usuários</div>
+    const [users, setUsers] = useState(null);
+
+    // Load Users
+    useEffect(() => {
+
+        const loadUsers = async () => {
+            const res = await partyFetch.get("/users");
+
+            console.log(res);
+
+            setUsers(res.data);
+        };
+
+        loadUsers();
+
+    }, []);
+
+    if(!users) return <p>Carregando...</p>;
+
+    return <div>
+        <div>
+            {users.length === 0 && <p>Não há Usuários Cadastrados!</p>}
+            {users.map((user) => (
+                <div key={user._id}>
+                    <p>Nome: {user.name}</p>
+                    <p>Email: {user.email}</p>
+                </div>
+            ))}
+        </div>
+    </div>
 };
 
 export default Users;
