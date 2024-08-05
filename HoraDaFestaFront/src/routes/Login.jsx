170b813c98eba615d009da8b9866e03df677c82c
erikgomessantos <97444977+erikgomessantos@users.noreply.github.com>
2024-08-05 20:22:08 -0300

    Ajuste de telas de Login e Cadastro

--- HoraDaFestaFront/src/routes/Login.jsx
+++ HoraDaFestaFront/src/routes/Login.jsx
@@ -1,30 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import imagemGoogle from "../img/google.png";
+import "./login.css";
 
 const Login = () => {
-    return <div class="container">
-        <h1 class="login-title">Hora da Festa</h1>
-        <div class="login-container">
-        <h2>Login</h2>
-        <form onSubmit={(e) => createUser(e)}>
-            <label>
-                <span>Nome de Usuário</span>
-                <input type="text" placeholder="Nome" 
-                required 
-                />
-            </label>
-            <label>
-                <span>Senha</span>
-                <input type="text" placeholder="Senha" 
-                required
-                />
-            </label>
-            <input type="submit" value="Login" className="btn"/>
-        </form>
-        <p>Ou</p>
-        <input type="submit" value="Fazer Login com google"/>
-        <p>Não tem uma conta? Cadastrar</p>
-        </div>
-    </div>
+    return <section className="center-page">
+        <div className="form-page">
+            <h2>Hora da Festa</h2>
+            <h3>Login</h3>
+            <form>
+                <label>
+                    <span>Email</span>
+                    <input type="text" placeholder="Email" 
+                    required
+                    />
+                </label>
+                <label>
+                    <span>Senha</span>
+                    <input type="text" placeholder="Senha" 
+                    required
+                    />
+                </label>
+                <input type="submit" value="Login" className="btn"/>
+                <p>Ou</p>
+                <p className="login-google"><img src={imagemGoogle}/>Fazer login com o Google</p>
+                <p>Não tem uma conta? <Link to="/register">Cadastrar</Link></p>
+            </form>
+        </div>;
+    </section> 
 };
 
 export default Login;
--- HoraDaFestaFront/src/routes/Register.jsx
+++ HoraDaFestaFront/src/routes/Register.jsx
@@ -1,33 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./login.css";
 
 const Register = () => {
-    return <div class="container">
-        <h1 class="login-title">Hora da Festa</h1>
-        <div class="login-container">
-            <h2>Login</h2>
-        <form onSubmit={(e) => createUser(e)}>
-            <label>
-                <span>Nome de Usuário</span>
-                <input type="text" placeholder="Nome" 
-                required 
-                />
-            </label>
-            <label>
-                <span>Email</span>
-                <input type="text" placeholder="Email" 
-                required 
-                />
-            </label>
-            <label>
-                <span>Senha</span>
-                <input type="text" placeholder="Senha" 
-                required
-                />
-            </label>
-            <input type="submit" value="Registrar" className="btn"/>
-        </form>
-        </div>
-    </div>
+    return <section className="center-page">
+        <div className="form-page">
+            <h2>Hora da Festa</h2>
+            <h3>Cadastrar</h3>
+            <form>
+                <label>
+                    <span>Nome</span>
+                    <input type="text" placeholder="Nome" 
+                    required
+                    />
+                </label>
+                <label>
+                    <span>Email</span>
+                    <input type="text" placeholder="Email" 
+                    required
+                    />
+                </label>
+                <label>
+                    <span>Senha</span>
+                    <input type="text" placeholder="Senha" 
+                    required
+                    />
+                </label>
+                <input type="submit" value="Cadastrar" className="btn"/>
+                <p>Ou</p>
+                <p>Já possui conta? <Link to="/login">Fazer Login</Link></p>
+            </form>
+        </div>;
+    </section> 
 };
 
 export default Register;
