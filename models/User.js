bb899c5db491e58afb556d5dae2f1ddbff3f2a38
erikgomessantos <97444977+erikgomessantos@users.noreply.github.com>
2024-08-07 06:27:21 -0300

    Refatorando os models

--- models/User.js
+++ models/User.js
@@ -1,22 +1,17 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-}, { timestamps: true }
+const userSchema = new Schema(
+  {
+    name: String,
+    email: String,
+    password: String,
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const User = mongoose.model("User", userSchema);
+User = mongoose.model("User", userSchema);
 
 module.exports = User;
