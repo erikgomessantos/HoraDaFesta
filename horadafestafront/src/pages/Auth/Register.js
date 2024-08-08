90415d19bec23b4d35f2940d8cca4719130c28dc
erikgomessantos <97444977+erikgomessantos@users.noreply.github.com>
2024-08-08 20:51:31 -0300

    Ajustes na tela de Login e Cadastro

--- horadafestafront/src/pages/Auth/Register.js
+++ horadafestafront/src/pages/Auth/Register.js
@@ -10,38 +10,50 @@ import { useState, useEffect } from "react";
 
 const Register = () => {
 
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
     const handleSubmit = (e) => {
         e.preventDefault();
-    }
+
+        const user = {
+            name,
+            email,
+            password,
+            confirmPassword,
+        };
+    }; 
 
     return (
-        <div>
+        <div id="register">
             <h2>Hora da Festa</h2>
             <p className="subtitle">Cadastre-se para criar suas festas</p>
             <form onSubmit={handleSubmit}>
                 <input
                 type="text"
                 placeholder="Nome"
-                // onChange={(e) => setName(e.target.value)}
-                // value={name}
+                onChange={(e) => setName(e.target.value)}
+                value={name}
                 />
                 <input
                 type="email"
                 placeholder="E-mail"
-                // onChange={(e) => setEmail(e.target.value)}
-                // value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
                 />
                 <input
                 type="password"
                 placeholder="Senha"
-                // onChange={(e) => setPassword(e.target.value)}
-                // value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
                 />
                 <input
                 type="password"
                 placeholder="Confirme a senha"
-                // onChange={(e) => setConfirmPassword(e.target.value)}
-                // value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                value={confirmPassword}
                 />
                 {/* {!loading && <input type="submit" value="Cadastrar" />}
                 {loading && <input type="submit" disabled value="Aguarde..." />}
