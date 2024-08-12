1bf8f5c4276ae4db652b4d3fee82c8e92a88e56c
erikgomessantos <97444977+erikgomessantos@users.noreply.github.com>
2024-08-12 19:52:03 -0300

    Refatorando os services no frontend

--- horadafestafront/src/pages/Auth/Register.js
+++ horadafestafront/src/pages/Auth/Register.js
@@ -1,4 +1,4 @@
-import "../Auth/Auth.css";
+import "./Auth.css";
 
 // Components
 import { Link } from "react-router-dom";
@@ -6,61 +6,79 @@ import { Link } from "react-router-dom";
 
 // Hooks
 import { useState, useEffect } from "react";
-// import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+
+// Redux
+import { register, reset } from "../../slices/authSlice";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
+  const dispatch = useDispatch();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const { loading, error } = useSelector((state) => state.auth);
 
-        const user = {
-            name,
-            email,
-            password,
-            confirmPassword,
-        };
-    }; 
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    return (
-        <div id="register">
-            <h2>Hora da Festa</h2>
-            <p className="subtitle">Cadastre-se para criar suas festas</p>
-            <form onSubmit={handleSubmit}>
-                <input
-                type="text"
-                placeholder="Nome"
-                onChange={(e) => setName(e.target.value)}
-                value={name}
-                />
-                <input
-                type="email"
-                placeholder="E-mail"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-                />
-                <input
-                type="password"
-                placeholder="Senha"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-                />
-                <input
-                type="password"
-                placeholder="Confirme a senha"
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                value={confirmPassword}
-                />
-                {/* {!loading && <input type="submit" value="Cadastrar" />}
-                {loading && <input type="submit" disabled value="Aguarde..." />}
-                {error && <Message msg={error} type="error" />} */}
-            </form>
-        </div>
-    )
-}
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword,
+    };
 
-export default Register
+    console.log(user);
+
+    dispatch(register(user));
+  };
+
+  // Clean all auth states
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch]);
+
+  return (
+    <div id="register">
+      <h2>Hora Da Festa</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Nome"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <input
+          type="email"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <input
+          type="password"
+          placeholder="Confirme a senha"
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          value={confirmPassword}
+        />
+        {!loading && <input type="submit" value="Cadastrar" />}
+        {loading && <input type="submit" disabled value="Aguarde..." />}
+        {/* {error && <Message msg={error} type="error" />} */}
+      </form>
+      <p>
+        Já tem conta? <Link to="/login">Clique aqui</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Register;
