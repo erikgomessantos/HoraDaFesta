4c17cc61621944990b08413bea521a43702355d7
erikgomessantos <97444977+erikgomessantos@users.noreply.github.com>
2024-09-09 22:39:43 -0300

    Tela de edição de contatos

--- horadafesta/src/pages/EditContacts/EditContacts.js
+++ horadafesta/src/pages/EditContacts/EditContacts.js
@@ -1,85 +1,51 @@
-import "../EditParty/EditParty.css";
+import "../Contacts/Contacts.css";
+
+// 09-09-2024
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useAuthValue } from "../../context/AuthContext";
 import { UserUpdateParty } from "../../hooks/UserUpdateParty";
 import { UserFetchParty } from "../../hooks/UserFetchParty";
+// 09-09-2024
 
-// 05-09-2024
-import { UserFetchParties } from "../../hooks/UserFetchParties";
-// 05-09-2024
+const EditContact = () => {
 
-
-const EditParty = () => {
+    // 09-09-2024
     const {id} = useParams();
-    const {document: party} = UserFetchParty("parties", id);
+    const {document: contacts} = UserFetchParty("contacts", id);
 
-    // 05-09-2024
-    const {documents: contacts} = UserFetchParties("contacts", null);
-    // 05-09-2024
-
-    const [title, setTitle] = useState("");
-    const [image, setImage] = useState("");
-    
-    //04-09-2024
     const [name, setName] = useState("");
-    const [attractionsName, setAttractionsName] = useState("");
-    const [tasksDescription, setTasksDescription] = useState("");
-    const [places, setPlaces] = useState("");
-    const [suppliers, setSuppliers] = useState("");
-    //04-09-2024
-
+    const [nickname, setNickname] = useState("");
+    const [email, setEmail] = useState("");
+    const [telephone, setTelephone] = useState("");
     const [formError, setFormError] = useState("");
 
     useEffect(() => {
-        if(party) {
+        if(contacts) {
 
-            setTitle(party.title)
-            setImage(party.image)
-            
-            //04-09-2024
-            setName(party.name)
-            setAttractionsName(party.attractionsName)
-            setTasksDescription(party.tasksDescription)
-            setPlaces(party.places)
-            setSuppliers(party.suppliers)
-            //04-09-2024
+            setName(contacts.name)
+            setNickname(contacts.nickname)
+            setEmail(contacts.email)
+            setTelephone(contacts.telephone)
         }
-    }, [party])
-    
+    }, [contacts])
+
     const {user} = useAuthValue();
-    const {updateDocument, response} = UserUpdateParty("parties");
+    const {updateDocument, response} = UserUpdateParty("contatcs");
     const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
         setFormError("");
 
-        // URL Validation
-        try {
-
-            new URL(image)
-
-        } catch (error) {
-            
-            setFormError("A imagem precisa ser uma URL.")
-        };
-
         // Verificar todos os valores
         if(formError) return;
 
         const data = {
-            title,
-            image,
-            
-            // 04-09-2024
             name,
-            attractionsName,
-            tasksDescription,
-            places,
-            suppliers,
-            // 04-09-2024
-
+            nickname,
+            email,
+            telephone,
             uid: user.uid,
             createdBy: user.displayName
         };
@@ -87,140 +53,67 @@ const EditParty = () => {
        updateDocument(id, data);
 
         // Redirect to home
-        navigate("/dashboard");
+        navigate("/contacts");
     };
 
-    // 05-09-2024
-    // const [options] = useState([""]);
-    // 05-09-2024
-    
     return (
-        <div className="edit_party">
-            {party && (
+        <div className="create_contact">
+            {contacts && (
                 <>
-                    <h2>Editar Festa</h2>
-                    <p>Altere as informações das suas festas da forma que preferir!</p>
-                    <form onSubmit={handleSubmit}>
-                        <label>
-                            <span>Título:*</span>
-                            <input type="text"
-                                name="title"
-                                placeholder="Título da sua Festa"
-                                required 
-                                onChange={(e) => setTitle(e.target.value)}
-                                value={title}  
-                            />
-                        </label>
-                        <label>
-                            <span>URL da Imagem:*</span>
-                            <input type="text"
-                                name="image"
-                                placeholder="Insira o endereço da imagem que tem a sua festa"
-                                required 
-                                onChange={(e) => setImage(e.target.value)}
-                                value={image}  
-                            />
-                        </label>
-                        <p className="title_preview">Imagem atual:</p>
-                        <img
-                         className="image_preview"
-                         src={party.image}
-                         alt={party.title}
+                <h2>Cadastrar Contatos</h2>
+            {<form onSubmit={handleSubmit}>
+                    <label>
+                        <span>Nome:*</span>
+                        <input type="text"
+                            name="name"
+                            placeholder="Nome do contato"
+                            required 
+                            onChange={(e) => setName(e.target.value)}
+                            value={name}  
                         />
-                        
-                        {/* 04-09-2024 */}
-                        {/* <label>
-                            <span>Nome do Contato:</span>
-                            <input type="text"
-                                name="name"
-                                placeholder="Informe o nome do seu contato"
-                                onChange={(e) => setName(e.target.value)}
-                                value={name}  
-                            />
-                        </label> */}
-
-                        {/* 05-09-2024 */}
-                        {/* {contacts && contacts.map((contacts) => <p key={contacts.id} contacts={contacts}>
-                        <select>
-                            <option>Selecione os Contatos</option>
-                                {options.map(option => (
-                                <option key={contacts.id} value={contacts.name}>
-                                    {contacts.name}
-                                </option>
-                            ))}
-                        </select>
-                        </p>)}     */}
-                        
-                        <select 
-                        name="name"
-                        multiple={false}
-                        onChange={(e) => setName(e.target.value)}
-                        // value={[name]}
-                        >
-                            <option>Selecione os Contatos</option>
-                            {contacts.map((contacts, i) => {
-                                return <option key={i}>{contacts.name}</option>
-                            })}
-                        </select>
-
-                        
-                        {/* 05-09-2024 */}
-
-                        <label>
-                            <span>Atrações:</span>
-                            <input type="text"
-                                name="attractionsName"
-                                placeholder="Descreva suas atrações"
-                                onChange={(e) => setAttractionsName(e.target.value)}
-                                value={attractionsName}  
-                            />
-                        </label>
-
-                        <label>
-                            <span>Tarefas:</span>
-                            <input type="text"
-                                name="tasksDescription"
-                                placeholder="Descreva suas tarefas para que não esqueça de nada"
-                                onChange={(e) => setTasksDescription(e.target.value)}
-                                value={tasksDescription}  
-                            />
-                        </label>
-
-                        <label>
-                            <span>Locais:</span>
-                            <input type="text"
-                                name="places"
-                                placeholder="Cadastre seus lugares favoritos para comemorar suas festas"
-                                onChange={(e) => setPlaces(e.target.value)}
-                                value={places}  
-                            />
-                        </label>
-
-                        <label>
-                            <span>Fornecedores:</span>
-                            <input type="text"
-                                name="suppliers"
-                                placeholder="Cadastre seus fornecedores"
-                                onChange={(e) => setSuppliers(e.target.value)}
-                                value={suppliers}  
-                            />
-                        </label>
-                        {/* 04-09-2024 */}
-
-                        {!response.loading && <button className="btn">Editar</button>}
-                        {response.loading && (
-                            <button className="btn" disabled>
-                                Aguarde.. .
-                            </button>
-                        )}
-                        {(response.error || formError) && (
-                            <p className="error">{response.error || formError}</p>
-                        )}
-                    </form>
+                    </label>
+                    <label>
+                        <span>Apelido:</span>
+                        <input type="text"
+                            name="nickname"
+                            placeholder="Apelido"
+                            onChange={(e) => setNickname(e.target.value)}
+                            value={nickname}  
+                        />
+                    </label>
+                    <label>
+                        <span>E-mail:*</span>
+                        <input type="email"
+                            name="email"
+                            placeholder="Insira o e-mail do contato"
+                            required 
+                            onChange={(e) => setEmail(e.target.value)}
+                            value={email}  
+                        />
+                    </label>
+                    <label>
+                        <span>Telefone:</span>
+                        <input type="string"
+                            name="telephone"
+                            placeholder="(11)1234-5678"
+                            onChange={(e) => setTelephone(e.target.value)}
+                            value={telephone}  
+                        />
+                    </label>
+                    {!response.loading && <button className="btn">Editar</button>}
+                    {response.loading && (
+                        <button className="btn" disabled>
+                            Aguarde.. .
+                        </button>
+                    )}
+                    {(response.error || formError) && (
+                        <p className="error">{response.error || formError}</p>
+                    )}
+                </form>}
                 </>
-            )}
+            )}        
         </div>
-    );
+    )
 };
 
-export default EditParty;
+export default EditContact;
