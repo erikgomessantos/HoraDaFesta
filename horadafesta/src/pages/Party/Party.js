6b37d920b7cdbb08390731676add126941358a5d
erikgomessantos <97444977+erikgomessantos@users.noreply.github.com>
2024-09-23 19:18:05 -0300

    Ajuste de export do excel

--- horadafesta/src/pages/Party/Party.js
+++ horadafesta/src/pages/Party/Party.js
@@ -2,9 +2,21 @@ import "../Party/Party.css";
 import { useParams } from "react-router-dom";
 import { UserFetchParty } from "../../hooks/UserFetchParty";
 
+import { ExportCSV } from "../../components/ExportCSV";
+
 const Party = () => {
     const {id} = useParams();
     const {document: party, loading} = UserFetchParty("parties", id);
+    let viewers = [];
+    
+    if(party) {
+         viewers = [
+            // {Título:1,Contatos:2,Atrações:3,Tarefas:4,Locais:5, Fornecedores:6}
+            {Título: party.title, Nome: party.name},
+            {Atrações: party.attractionsName}
+        
+        ]
+    }
 
     return (
         <div className="party_container">
@@ -19,6 +31,9 @@ const Party = () => {
                     <p>Local: {party.places}</p>
                     <p>Fornecedores: {party.suppliers}</p>
                     <p className="createdby">Festa criada por:{party.createdBy}</p>
+                    {viewers.length > 0 && (
+                        <ExportCSV csvData={viewers} fileName={party.title} />
+                    )}    
                 </>
             )}
         </div>
